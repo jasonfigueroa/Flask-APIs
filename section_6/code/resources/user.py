375bcd51349d13734755ddbb6d37a8feaa9fe5a9
Jason Figueroa <jason.figueroa2@gmail.com>
2018-02-13 22:00:07 -0600

    installed and configured SQLAlchemy

--- section_6/code/resources/user.py
+++ section_6/code/resources/user.py
@@ -1,57 +1,7 @@
 import sqlite3
 from flask_restful import Resource, reqparse
+from models.user import UserModel
 
-class User:
-	def __init__(self, _id, username, password):
-		self.id = _id
-		self.username = username
-		self.password = password
-
-	@classmethod
-	def find_by_username(cls, username):
-		connection = sqlite3.connect('data.db')
-		cursor = connection.cursor()
-
-		query = 'SELECT * FROM users WHERE username=?'
-		
-		# the second parameter is a tuple of length 1
-		result = cursor.execute(query, (username,))
-		
-		row = result.fetchone()
-
-		if row:
-			# the following is the same as the next next line
-			# user = cls(row[0], row[1], row[2])			
-			user = cls(*row) # *row is a positional argument
-		else:
-			user = None
-
-		connection.close()
-		
-		return user
-
-	@classmethod
-	def find_by_id(cls, _id):
-		connection = sqlite3.connect('data.db')
-		cursor = connection.cursor()
-
-		query = 'SELECT * FROM users WHERE id=?'
-		
-		# the second parameter is a tuple of length 1
-		result = cursor.execute(query, (_id,))
-		
-		row = result.fetchone()
-
-		if row:
-			# the following is the same as the next next line
-			# user = cls(row[0], row[1], row[2])			
-			user = cls(*row) # *row is a positional argument
-		else:
-			user = None
-
-		connection.close()
-		
-		return user
 
 class UserRegister(Resource):
 	parser = reqparse.RequestParser()
@@ -67,16 +17,17 @@ class UserRegister(Resource):
 	)
 	def post(self):
 		data = UserRegister.parser.parse_args()
-		if User.find_by_username(data['username']):
+		if UserModel.find_by_username(data['username']):
 			return {"message": "User with that name already exists."}, 400
 			
-		connection = sqlite3.connect('data.db')
-		cursor = connection.cursor()
+		# user = UserModel(data['username'], data['password'])
+		
+		# the following line is essentially the same as the previous line and 
+		# can be used in this way due to the parser and the way it's configured 
+		# at the top of this class
 
-		query = "INSERT INTO users VALUES (NULL, ?, ?)"
-		cursor.execute(query, (data['username'], data['password']))
+		user = UserModel(**data)
 
-		connection.commit()
-		connection.close()
+		user.save_to_db()
 
 		return {"message": "User created successfully."}, 201
